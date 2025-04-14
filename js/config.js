06757686cc82321e25dfe589e978600e826feb04
Ruslana Tomniuk <ruslana@r-tomniuk-P.local>
2025-04-14 16:11:44 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: 'eu-central-1_KvvJy7iPR', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '1alf99jlsjc745lli96q1bdj92', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'eu-central-1', // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: '', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
